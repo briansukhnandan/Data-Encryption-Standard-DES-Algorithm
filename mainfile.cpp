c0fd645092fa47dde92b65e85fef74100196a682
Brian Sukhnandan <briansukhnandan@gmail.com>
2019-02-07 19:43:21 -0500

    Added more functions

--- mainfile.cpp
+++ mainfile.cpp
@@ -5,14 +5,14 @@
 using namespace std;
 
 //Global variables for DES initial permutation
-int initialPerm[64] = { 57, 49, 41, 33, 25, 17, 9, 1,
-                        59, 51, 43, 35, 27, 19, 11, 3,
-                        61, 53, 45, 37, 29, 21, 13, 5,
-                        63, 55, 47, 39, 31, 23, 15, 7,
-                        56, 48, 40, 32, 24, 16, 8, 0,
-                        58, 50, 42, 34, 26, 18, 10, 2,
-                        60, 52, 44, 36, 28, 20, 12, 4,
-                        62, 54, 46, 38, 30, 22, 14, 6 };
+int initialPermTable[64] = { 57, 49, 41, 33, 25, 17, 9, 1,
+                        	 59, 51, 43, 35, 27, 19, 11, 3,
+                        	 61, 53, 45, 37, 29, 21, 13, 5,
+                        	 63, 55, 47, 39, 31, 23, 15, 7,
+                        	 56, 48, 40, 32, 24, 16, 8, 0,
+                        	 58, 50, 42, 34, 26, 18, 10, 2,
+                        	 60, 52, 44, 36, 28, 20, 12, 4,
+                        	 62, 54, 46, 38, 30, 22, 14, 6 };
                         
 
 void numberToBinary(int x) {
@@ -44,6 +44,27 @@ void numberToBinary(int x) {
     
 }
 
+string boolvectoString(vector<bool> x) {
+	
+	string z = "";
+	
+	for (int i = 0; i < 64; i++) {
+		
+		if (x[i] == true) {
+			
+			z += "1";
+			
+		}else {
+			
+			z += "0";
+			
+		}
+		
+		
+	}
+	return z;
+}
+
 string stringToBinary(string x) {
     
     string y = "";
@@ -54,7 +75,9 @@ string stringToBinary(string x) {
         y += bitset<8>(x.c_str()[i]).to_string();
         
     }
-    
+    //Returns a string of the binary representation.
+    //In c++, since strings can be treated as an array,
+    //this seems like a suitable choice.
     return y;
 }
 
@@ -77,21 +100,62 @@ void insertBinaryVector(string y, vector<bool> &z) {
     
 }
 
-void initialPermutation(vector<int> x) {
+//In the initial permutation, the 64 bit key we had will now be reduced to 56 bits.
+vector<bool> initialPermutation(vector<bool> x) {
+	
+	string z = boolvectoString(x);
+	cout << "Bool Vector put back into a string: " << endl;
+	for (int j = 0; j < 64; j++) {
+		
+		cout << z[j];
+		
+		if ((j+1) % 8 == 0) {
+			cout << endl;
+		}
+		
+	}
+	cout << endl;
+//	cout << "Bool Vector put back into a string: " <<  z << endl;
+	
+    //Original vector size is 64.
+    //This new vector<bool> y will contain 56 bits (elements).
+    vector<bool> y;
+    
+    //The 
+    for (int i = 0; i < 56; i++) {
+    	
+    	if ( (z[initialPermTable[i]]) == '1') {
+    		
+    		y.push_back(true);
+    		
+		} else {
+			
+			y.push_back(false);
+			
+		}
+    	
+    	
+    	
+	}
     
     
-    
-    
+    return y;
 }
 
-//Key we're working with: IEOFIT#1
+//Key we're working with: ABCDEFGH
 //In binary this is : which is 64 bits long.
-//01001001 01000101 01001111 01000110 
-//01001001 01010100 00100011 00110001
+// 01000001
+// 01000010
+// 01000011
+// 01000100
+// 01000101
+// 01000110
+// 01000111
+// 01001000
 
 int main() {
     
-    string stringKey = "IEOFIT#1";
+    string stringKey = "ABCDEFGH";
     
     string newBinaryKey = stringToBinary(stringKey);
     cout << "newBinaryKey " << newBinaryKey << endl;
@@ -100,14 +164,31 @@ int main() {
     
     insertBinaryVector(newBinaryKey, initialKeyVector);
     
-    cout << "Vector of bool values: ";
+    
+    cout << endl << "Vector of bool values: ";
     
     for (int i = 0; i < initialKeyVector.size(); i++) {
         
         cout << initialKeyVector[i];
         
     }
-      
+    cout << endl;
+	cout << endl;
+	
+	vector<bool> firstPermutatedVec = initialPermutation(initialKeyVector);  
+	
+	cout << "First permutation key: " << endl;
+	for (int j = 0; j < 64; j++) {
+		
+		cout << firstPermutatedVec[j];
+		
+		if ((j+1) % 8 == 0) {
+			cout << endl;
+		}
+		
+		
+	}
+	cout << endl;
       
     return 0;
 }
